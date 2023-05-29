d07c7715a80dd8096f8f4ef9cfe39b55ae9cab5c
Shiny <zhushiny@gmail.com>
2023-05-29 12:54:48 +0800

    Update shortener.sh

--- shortener.sh
+++ shortener.sh
@@ -1,13 +1,16 @@
+
+api="https://yourworker.subdomain.workers.dev/"
+
 # Check if argument is provided
 if [ $# -eq 0 ]
   then
     echo "No URL provided"
-    echo "shortener.sh <URL>"
+    echo "Usage: shortener.sh <URL>"
     exit 1
 fi
 
 # Send curl request
-data='{"url": "'"$1"'","secret": "your_own_secret"}'
+data='{"url": "'$1'", "secret": "your_own_secret"}'
 
-curl -X POST -d "$data" shiny.im
+curl -X POST -d "$data" $api
 
